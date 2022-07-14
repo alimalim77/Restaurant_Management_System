afd92bfde8b8c1283393871f94fe104576ca65d2
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-07-14 18:47:45 +0530

    Change

--- appone.js
+++ appone.js
@@ -1,8 +1,96 @@
+// const fs = require('fs');
+// const path = require('path');
+
+// const express = require('express');
+// const uuid = require('uuid');
+
+// //const resData = require('./util/restaurant-data');
+
+// const app = express();
+
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+// app.use(express.static('public'));
+// app.use(express.urlencoded({ extended: false }));
+
+// app.get('/', function (req, res) {
+//   res.render('index');
+// });
+
+// app.get('/restaurants', function (req, res) {
+//   const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+//   const fileData = fs.readFileSync(filePath);
+//   const storedRestaurants = JSON.parse(fileData);
+
+//   res.render('restaurants', {
+//     numberOfRestaurants: storedRestaurants.length,
+//     restaurants: storedRestaurants,
+//   });
+// });
+
+// app.get('/restaurants/:id', function (req, res) {
+//   const restaurantId = req.params.id;
+//   const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+//   const fileData = fs.readFileSync(filePath);
+//   const storedRestaurants = JSON.parse(fileData);
+//   for (const restaurant of storedRestaurants){
+//     if (restaurant.id === restaurantId){
+//       res.render('restaurant-detail', { restaurant: restaurant });
+//     }
+//   }
+
+//   res.status(404).render('404')
+  
+// });  
+
+// app.get('/recommend', function (req, res) {
+//   res.render('recommend');
+// });
+
+// app.post('/recommend', function (req, res) {
+//   const restaurant = req.body;
+//   restaurant.id = uuid.v4();
+//   const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+//   const fileData = fs.readFileSync(filePath);
+//   const storedRestaurants = JSON.parse(fileData);
+
+//   storedRestaurants.push(restaurant);
+
+//   fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+//   res.redirect('/confirm');
+// });
+
+// app.get('/confirm', function (req, res) {
+//   res.render('confirm');
+// });
+
+// app.get('/about', function (req, res) {
+//   res.render('about');
+// });
+
+
+// app.use(function(req,res) {
+//   res.status(404).render('404');
+// });
+
+// app.use(function (error, req, res, next){
+//     res.status(500).render('500');
+// });
+
+// app.listen(3000);
+
 const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
+const uuid = require('uuid');
 
+const resData = require('./util/restaurant-data');
 
 const app = express();
 
@@ -17,10 +105,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/restaurants', function (req, res) {
-  const filePath = path.join(__dirname, 'data', 'restaurants.json');
-
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  const storedRestaurants = resData.getStoredRestaurants();
 
   res.render('restaurants', {
     numberOfRestaurants: storedRestaurants.length,
@@ -30,9 +115,15 @@ app.get('/restaurants', function (req, res) {
 
 app.get('/restaurants/:id', function (req, res) {
   const restaurantId = req.params.id;
-  res.render('restaurant-detail', { rid: restaurantId });
+  const storedRestaurants = resData.getStoredRestaurants();
 
-  
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render('restaurant-detail', { restaurant: restaurant });
+    }
+  }
+
+  res.render('404');
 });
 
 app.get('/recommend', function (req, res) {
@@ -41,14 +132,12 @@ app.get('/recommend', function (req, res) {
 
 app.post('/recommend', function (req, res) {
   const restaurant = req.body;
-  const filePath = path.join(__dirname, 'data', 'restaurants.json');
+  restaurant.id = uuid.v4();
+  const restaurants = resData.getStoredRestaurants();
 
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
+  restaurants.push(restaurant);
 
-  storedRestaurants.push(restaurant);
-
-  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+  resData.storeRestaurants(restaurants);
 
   res.redirect('/confirm');
 });
@@ -59,6 +148,14 @@ app.get('/confirm', function (req, res) {
 
 app.get('/about', function (req, res) {
   res.render('about');
+});
+
+app.use(function (req, res) {
+  res.render('404');
+});
+
+app.use(function (error, req, res, next) {
+  res.render('500');
 });
 
 app.listen(3000);
